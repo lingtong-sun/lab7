3c4460fd8c11270cdd92a64fd06c646b8889f542
Lingtong Sun <lsun569@stanford.edu>
2014-02-18 23:32:36 -0800

    database lab

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,12 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
   // query for the specific project and
   // call the following callback
+
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,6 +21,19 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+      "title": form_data['project_title'],
+      "date": form_data['date'],
+      "summary": form_data['summary'],
+      "image": form_data['image_url']
+  });
+  newProject.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) {console.log(err);res.send(500);}
+    res.redirect('/');
+  }
+
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +41,17 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(afterRemoving);
+
+    function afterRemoving(err) {
+      if(err) console.log(err);
+      res.send();
+    }
+
+
+
 }
